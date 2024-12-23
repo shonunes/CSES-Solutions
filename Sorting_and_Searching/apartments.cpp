59e8eb6137b7247719650dc0f895bab324e702e1
Daniel Nunes <daniel.msnunes@gmail.com>
2024-12-23 22:51:18 +0000

    Sorting and Searching problems

---
+++ Sorting_and_Searching/apartments.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<bool>> vvb;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define endl '\n'
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define INF 2e9
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define cut(str, s, e) (string(str.begin() + s, str.end() + e))
+#define pqTopMaior(t) priority_queue<t, vector<t>, less<t>>
+#define pqTopMenor(t) priority_queue<t, vector<t>, greater<t>>
+
+inline int mod(int a, int m) { return ((a % m) + m) % m; }
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int n_applicants, n_apartments, k;
+    cin >> n_applicants >> n_apartments >> k;
+    
+    vi desires(n_applicants), sizes(n_apartments);
+    forn(i, n_applicants)
+        cin >> desires[i];
+    forn(i, n_apartments)
+        cin >> sizes[i];
+    sort(desires.begin(), desires.end());
+    sort(sizes.begin(), sizes.end());
+
+    int applicant = 0, apartment = 0, ans = 0;
+    while (applicant != n_applicants && apartment != n_apartments)
+    {
+        if (sizes[apartment] < desires[applicant] - k)
+            apartment++;
+        else if (sizes[apartment] > desires[applicant] + k)
+            applicant++;
+        else
+        {
+            apartment++;
+            applicant++;
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
