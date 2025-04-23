fa7f3152057252fa6e403c58d97f2bc6ac9202c0
shonunes <daniel.msnunes@gmail.com>
2025-04-23 23:54:14 +0100

    Towers

---
+++ Sorting_and_Searching/towers.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    multiset<int> towerTops;
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int cube;
+        cin >> cube;
+
+        auto it = towerTops.upper_bound(cube);
+        
+        if (it == towerTops.end())
+        {
+            towerTops.insert(cube);
+            ans++;
+        }
+        else
+        {
+            towerTops.erase(it);
+            towerTops.insert(cube);
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
