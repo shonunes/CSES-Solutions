3055908f8f03c423cd24c7a629a29f7a1a171169
shonunes <daniel.msnunes@gmail.com>
2025-05-31 22:58:59 +0100

    Tasks and Deadlines

---
+++ Sorting_and_Searching/tasks_and_deadlines.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<bool>> vvb;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define endl '\n'
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define INF 2e9
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define cut(str, s, e) (string(str.begin() + s, str.end() + e))
+#define pqTopMaior(t) priority_queue<t, vector<t>, less<t>>
+#define pqTopMenor(t) priority_queue<t, vector<t>, greater<t>>
+
+inline int mod(int a, int m) { return ((a % m) + m) % m; }
+
+bool check(int t, ll time, vi &machines)
+{
+    ll products = 0;
+    for (int machine : machines)
+    {
+        products += time / machine;
+        if (products >= t)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> tasks(n);
+    forn(i, n)
+    {
+        cin >> tasks[i].first >> tasks[i].second;
+    }
+    sort(tasks.begin(), tasks.end());
+
+    ll ans = 0, t = 0;
+    for (pii task : tasks)
+    {
+        t += task.first;
+        ans += task.second - t;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
