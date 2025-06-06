b06784a44fc8b680fdb8adc62fcece25130ae8c0
shonunes <daniel.msnunes@gmail.com>
2025-06-06 14:39:59 +0100

    Sum of Four Values

---
+++ Sorting_and_Searching/sum_of_four_values.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<bool>> vvb;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define endl '\n'
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define INF 2e9
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define cut(str, s, e) (string(str.begin() + s, str.end() + e))
+#define pqTopMaior(t) priority_queue<t, vector<t>, less<t>>
+#define pqTopMenor(t) priority_queue<t, vector<t>, greater<t>>
+
+inline int mod(int a, int m) { return ((a % m) + m) % m; }
+
+struct info {
+    int sum, x, y;
+
+    info(int sum, int x, int y) : sum(sum), x(x), y(y) {}
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, target;
+    cin >> n >> target;
+
+    vi values(n);
+    forn(i, n)
+    {
+        cin >> values[i];
+    }
+
+    map<int, pii> m;
+    forsn(i, 2, n) {
+        int prev = values[i - 1];
+        forn(j, i - 1) {
+            
+            m[values[j] + prev] = {j + 1, i};
+        }
+        
+        forsn(j, i + 1, n) {
+            int sum = values[i] + values[j];
+
+            if (sum <= target)
+            {
+                auto it = m.find(target - sum);
+                if (it != m.end())
+                {
+                    cout << it->second.first << ' ' << it->second.second << ' ' << i + 1 << ' ' << j + 1 << endl;
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE\n";
+
+    return 0;
+}
