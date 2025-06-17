63be61427f04b868b2fc45c86becc2c29ff4d79c
shonunes <daniel.msnunes@gmail.com>
2025-06-17 10:44:57 +0100

    Counting Towers

---
+++ Dynamic_Programming/counting_towers.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<char>> vvc;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<bool>> vvb;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define endl '\n'
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define INF 2e9
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define cut(str, s, e) (string(str.begin() + s, str.end() + e))
+#define pqTopMaior(t) priority_queue<t, vector<t>, less<t>>
+#define pqTopMenor(t) priority_queue<t, vector<t>, greater<t>>
+
+inline ll mod(ll a, ll m) { return ((a % m) + m) % m; }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    vi mem(1e6 + 1);
+    mem[1] = 2;
+    ll together = 1, separated = 1;
+    forsn(i, 2, 1e6 + 1)
+    {
+        ll aux = mod(2 * together + separated, MOD);
+        separated = mod(together + 4 * separated, MOD);
+        together = aux;
+
+        mem[i] = mod(together + separated, MOD);
+    }
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << mem[n] << endl;
+    }
+
+    return 0;
+}
