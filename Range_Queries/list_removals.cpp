579a9bd82163d2aee22b64db09e041ff564f8ad0
shonunes <daniel.msnunes@gmail.com>
2025-07-15 17:40:02 +0100

    Range Queries Problems

---
+++ Range_Queries/list_removals.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<bool>> vvb;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define endl '\n'
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define INF 2e9
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define cut(str, s, e) (string(str.begin() + s, str.end() + e))
+#define pqTopMaior(t) priority_queue<t, vector<t>, less<t>>
+#define pqTopMenor(t) priority_queue<t, vector<t>, greater<t>>
+
+inline ll mod(ll a, ll m) { return ((a % m) + m) % m; }
+
+// See: Order-Statistic Tree
+using oset = tree<pii,       // key type
+                  null_type, // value type
+                  less<pii>, // compare function
+                  rb_tree_tag,
+                  tree_order_statistics_node_update>;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll n;
+    cin >> n;
+
+    auto ost = oset();
+    forn(i, n)
+    {
+        int num;
+        cin >> num;
+        ost.insert({i, num});
+    }
+
+    forn(i, n)
+    {
+        int pos;
+        cin >> pos;
+        auto it = ost.find_by_order(pos - 1);
+        cout << it->second << " ";
+        ost.erase(it);
+    }
+    cout << endl;
+
+    return 0;
+}
