eb9d571f6b0595894b0b013594ed0161c9bc57ef
shonunes <daniel.msnunes@gmail.com>
2025-12-05 09:22:45 +0000

    Tree Matching

---
+++ Tree_Algorithms/tree_matching.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<bool>> vvb;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define endl '\n'
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define INF 2e9
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define cut(str, s, e) (string(str.begin() + s, str.end() + e))
+#define pqTopMaior(t) priority_queue<t, vector<t>, less<t>>
+#define pqTopMenor(t) priority_queue<t, vector<t>, greater<t>>
+
+inline ll mod(ll a, ll m) { return ((a % m) + m) % m; }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<set<int>> adj(n);
+    vi counts(n);
+    forn(i, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        adj[a].insert(b);
+        adj[b].insert(a);
+        counts[a]++;
+        counts[b]++;
+    }
+
+    queue<pii> q;
+    forn(i, n)
+    {
+        if (counts[i] == 1)
+            q.push({i, *adj[i].begin()});
+    }
+
+    vb used(n);
+    int ans = 0;
+    while(!q.empty())
+    {
+        pii node = q.front();
+        q.pop();
+
+        if (used[node.first] || used[node.second])
+            continue;
+        used[node.first] = true;
+        used[node.second] = true;
+        ans++;
+
+        for (int nb : adj[node.second])
+        {
+            counts[nb]--;
+            adj[nb].erase(node.second);
+            if (counts[nb] == 1)
+                q.push({nb, *adj[nb].begin()});
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
