89db49b0c076f5bd705228e48a6cc802af82733f
shonunes <daniel.msnunes@gmail.com>
2025-12-05 22:11:41 +0000

    Tree Diameter

---
+++ Tree_Algorithms/tree_diameter.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<pii>> vvpii;
+typedef vector<vector<bool>> vvb;
+typedef vector<pll> vpll;
+typedef vector<pii> vpii;
+
+ll MOD = 1e9 + 7;
+double eps = 1e-12;
+const int di[] = {1, 0, -1, 0};
+const int dj[] = {0, -1, 0, 1};
+
+#define forn(i, e) for (ll i = 0; i < e; i++)
+#define forsn(i, s, e) for (ll i = s; i < e; i++)
+#define rforn(i, s) for (ll i = s; i >= 0; i--)
+#define rforsn(i, s, e) for (ll i = s; i >= e; i--)
+#define endl '\n'
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define INF 2e9
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define cut(str, s, e) (string(str.begin() + s, str.end() + e))
+#define pqTopMaior(t) priority_queue<t, vector<t>, less<t>>
+#define pqTopMenor(t) priority_queue<t, vector<t>, greater<t>>
+
+inline ll mod(ll a, ll m) { return ((a % m) + m) % m; }
+
+int dfs(int node, int parent, int &ans, vvi &adj)
+{
+    int max_depth = 0;
+    for (int nb : adj[node])
+    {
+        if (nb == parent)
+            continue;
+        
+        int depth = dfs(nb, node, ans, adj) + 1;
+        ans = max(ans, max_depth + depth);
+        max_depth = max(max_depth, depth);
+    }
+
+    return max_depth;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vvi adj(n);
+    forn(i, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+        a--; b--;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+
+    int ans = 0;
+    dfs(0, -1, ans, adj);
+    cout << ans << endl;
+
+    return 0;
+}
